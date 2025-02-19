ce83e753f675de8620925e90365bc31892f8e8ee
Ashwanth Vijayan <ashwanth@umich.edu>
2025-02-19 09:41:02 -0500

    Getting all the files in still not complete

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,39 +1,29 @@
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
-import { useFonts } from 'expo-font';
-import { Stack } from 'expo-router';
-import * as SplashScreen from 'expo-splash-screen';
-import { StatusBar } from 'expo-status-bar';
-import { useEffect } from 'react';
-import 'react-native-reanimated';
+import {SplashScreen, Stack} from "expo-router";
 
-import { useColorScheme } from '@/hooks/useColorScheme';
-
-// Prevent the splash screen from auto-hiding before asset loading is complete.
-SplashScreen.preventAutoHideAsync();
+import "./globals.css";
+import {useFonts} from "expo-font";
+import {useEffect} from "react";
 
 export default function RootLayout() {
-  const colorScheme = useColorScheme();
-  const [loaded] = useFonts({
-    SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
-  });
+  const [fontsLoaded] = useFonts( {
+    "Rubik-Bold": require('./fonts/Rubik-Bold.ttf'),
+    "Rubik-ExtraBold": require('./fonts/Rubik-ExtraBold.ttf'),
+    "Rubik-Light": require('./fonts/Rubik-Light.ttf'),
+    "Rubik-Medium": require('./fonts/Rubik-Medium.ttf'),
+    "Rubik-Regular": require('./fonts/Rubik-Regular.ttf'),
+    "Rubik-SemiBold": require('./fonts/Rubik-SemiBold.ttf'),
+  })
 
   useEffect(() => {
-    if (loaded) {
+    if (fontsLoaded) {
       SplashScreen.hideAsync();
-    }
-  }, [loaded]);
 
-  if (!loaded) {
+    }
+  }, [fontsLoaded]);
+
+  if (!fontsLoaded) {
     return null;
   }
 
-  return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="+not-found" />
-      </Stack>
-      <StatusBar style="auto" />
-    </ThemeProvider>
-  );
+  return <Stack />;
 }
